26067e54d2d02c79e18af27e1a20071ce94288a9
phhuy06 <134865180+phhuy06@users.noreply.github.com>
2024-10-02 20:02:00 +0700

    Update loang.js

--- src/components/Table/loang.js
+++ src/components/Table/loang.js
@@ -1,31 +1,28 @@
-const move = [-17, -1, 15, -16, 16, -15, 1, 17];
-function Loang( table, ind, number0, visited ) {
+export const move = [-17, -1, 15, -16, 16, -15, 1, 17];
+
+function Loang(table, ind, number0, visited) {
     for (let k = 0; k < 8; k++) {
-        if ((ind % 16 === 0 && k <= 2) || (ind % 16 === 15 && k >= 5)) {
+        if ((ind % 16 === 0 && (k === 1 || k === 2 || k === 5)) || (ind % 16 === 15 && (k === 0 || k === 3 || k === 4))) {
             continue;
         }
+
         const newind = ind + move[k];
+
         if (newind < 0 || newind > 255) {
             continue;
         }
-        if(table[newind].number === 0 && !visited[newind]){
-            visited[newind] = 1;
+
+        if (table[newind].number === 0 && !visited[newind]) {
+            visited[newind] = true;
             number0.push(newind);
-            Loang( table, newind, number0,visited );
-        }
-    }
-    for (let k = 0; k < 8; k++) {
-        if ((ind % 16 === 0 && k <= 2) || (ind % 16 === 15 && k >= 5)) {
-            continue;
-        }
-        const newind = ind + move[k];
-        if (newind < 0 || newind > 255) {
-            continue;
-        }
-        if(!table[newind].mine){
+            Loang(table, newind, number0, visited);
+        } 
+        else if (!table[newind].mine && !visited[newind]) {
+            visited[newind] = true;
             number0.push(newind);
         }
     }
+
     return number0;
 }
 
